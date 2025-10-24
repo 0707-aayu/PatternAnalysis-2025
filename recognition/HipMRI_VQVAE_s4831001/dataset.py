1737db69bc8a9965629921efa57e8e5631dd9a69
Aayushi-0707 <aayushidhande07@gmail.com>
2025-10-24 20:37:18 +1000

    Adding HipMRIDataset class for handling .nii.gz data with transforms

---
+++ recognition/HipMRI_VQVAE_s4831001/dataset.py
@@ -0,0 +1,46 @@
+import os
+from torch.utils.data import Dataset, DataLoader
+import torch
+from utils import load_data_2d
+
+class HipMRIDataset(Dataset):
+    """
+    PyTorch Dataset for HipMRI processed 2D slices
+    """
+
+    def __init__(self, file_list, norm_image=True):
+        """
+        Args:
+            file_list (list): List of full paths to .nii.gz files
+            norm_image (bool): Whether to normalize each slice
+        """
+        self.data = load_data_2d(file_list, norm_image=norm_image)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # Each element of self.data is a torch.Tensor of shape [1,H,W]
+        return self.data[idx]
+
+def get_dataloader(data_dir, batch_size=8, shuffle=True, norm_image=True, num_samples=None):
+    """
+    Helper function to get DataLoader for HipMRI 2D slices.
+
+    Args:
+        data_dir (str): Directory containing the .nii.gz slice files
+        batch_size (int): Batch size for DataLoader
+        shuffle (bool): Whether to shuffle the dataset
+        norm_image (bool): Normalize image slices
+        num_samples (int or None): Optional number of samples to limit loading
+
+    Returns:
+        DataLoader: PyTorch DataLoader for the dataset
+    """
+    file_list = [os.path.join(data_dir, f) for f in os.listdir(data_dir) if f.endswith('.nii.gz')]
+    if num_samples:
+        file_list = file_list[:num_samples]
+
+    dataset = HipMRIDataset(file_list, norm_image=norm_image)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+    return dataloader
