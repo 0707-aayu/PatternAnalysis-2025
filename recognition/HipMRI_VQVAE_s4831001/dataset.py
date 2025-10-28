b50bbb2562104e7d29f27e11135c4fefcc69b192
Aayushi-0707 <aayushidhande07@gmail.com>
2025-10-28 12:42:13 +1000

    feat(dataset): accept and apply transform; pass via get_dataloader

--- recognition/HipMRI_VQVAE_s4831001/dataset.py
+++ recognition/HipMRI_VQVAE_s4831001/dataset.py
@@ -8,20 +8,24 @@ class HipMRIDataset(Dataset):
     PyTorch Dataset for HipMRI processed 2D slices
     """
 
-    def __init__(self, file_list, norm_image=True):
+    def __init__(self, file_list, norm_image=True, transform=None):
         """
         Args:
             file_list (list): List of full paths to .nii files
             norm_image (bool): Whether to normalize each slice
         """
         self.data = load_data_2d(file_list, norm_image=norm_image)
+        self.transform = transform
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
+        x = self.data[idx]
+        if self.transform is not None:
+            x = self.transform(x)
         # Each element of self.data is a torch.Tensor of shape [1,H,W]
-        return self.data[idx]
+        return x
 
 def get_dataloader(data_dir, batch_size=8, shuffle=True, norm_image=True, num_samples=None):
     """
@@ -41,7 +45,8 @@ def get_dataloader(data_dir, batch_size=8, shuffle=True, norm_image=True, num_sa
     if num_samples:
         file_list = file_list[:num_samples]
 
-    dataset = HipMRIDataset(file_list, norm_image=norm_image)
+    dataset = HipMRIDataset(file_list, norm_image=norm_image, trasnform=trasnform)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
     return dataloader
+
 
